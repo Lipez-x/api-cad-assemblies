6bdfa6e3df5c15ec9beaa3c02ec5f374a0d7ca11
Felipe Lima <lipezera1836@gmail.com>
2024-12-02 11:31:56 -0300

    chore(auth) create module, controller & service

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -5,6 +5,7 @@ import { AllExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
+
   await app.listen(8080);
 }
 bootstrap();
