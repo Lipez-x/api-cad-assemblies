1f68e6892471c33f23c238cbb6a5642d80a94d18
Felipe Lima <lipezera1836@gmail.com>
2024-12-02 11:14:01 -0300

    feat: http exception filter

---
+++ api-gateway/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentsHost,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const request = context.getRequest();
+    const response = context.getResponse();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : exception;
+
+    response.status(status).json({
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error: message,
+    });
+  }
+}
