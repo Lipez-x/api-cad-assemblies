90cc2ab4b9a5a0b0e26802aa859ebe724f127dc8
Felipe Lima <lipezera1836@gmail.com>
2024-12-04 10:38:11 -0300

    feat(api-gateway): implemented departments service methods

--- api-gateway/src/department/department.service.ts
+++ api-gateway/src/department/department.service.ts
@@ -1,14 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateDepartmentDto } from './dtos/create-department.dto';
 import { UpdateDepartmentDto } from './dtos/update-department.dto';
+import { ClientProxyCadAssemblies } from 'src/proxyrmq/client-proxy';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class DepartmentService {
-  createDepartment(createDepartmentDto: CreateDepartmentDto) {}
-  async getDepartments(id: string) {}
-  async updateDepartment(
-    id: string,
-    updateDepartmentDto: UpdateDepartmentDto,
+  constructor(
+    private readonly clientProxyCadAssemblies: ClientProxyCadAssemblies,
   ) {}
-  async deleteDepartment(id: string) {}
+
+  private clientAdminBackend =
+    this.clientProxyCadAssemblies.getClientProxyAdminBackendInstance();
+
+  private logger = new Logger(DepartmentService.name);
+
+  createDepartment(createDepartmentDto: CreateDepartmentDto) {
+    try {
+      this.clientAdminBackend.emit('create-department', createDepartmentDto);
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async getDepartments(id: string) {
+    const department = await lastValueFrom(
+      this.clientAdminBackend.send('get-departments', id ? id : ''),
+    );
+
+    if (!department) {
+      throw new NotFoundException('Department not found');
+    }
+    try {
+      return department;
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+  async updateDepartment(id: string, updateDepartmentDto: UpdateDepartmentDto) {
+    const department = await lastValueFrom(
+      this.clientAdminBackend.send('get-departments', id),
+    );
+
+    if (!department) {
+      throw new NotFoundException('Department not found');
+    }
+
+    try {
+      this.clientAdminBackend.emit('update-department', {
+        id,
+        updateDepartmentDto,
+      });
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+  async deleteDepartment(id: string) {
+    const department = await lastValueFrom(
+      this.clientAdminBackend.send('get-departments', id),
+    );
+
+    if (!department) {
+      throw new NotFoundException('Department not found');
+    }
+
+    try {
+      this.clientAdminBackend.emit('delete-department', id);
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
 }
