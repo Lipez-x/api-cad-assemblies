05d83b7457758463284fd6829f3c3b198cce5eeb
Felipe Lima <lipezera1836@gmail.com>
2024-12-04 10:08:18 -0300

    feat(api-gateway): implemented departments controller methods

--- api-gateway/src/department/department.controller.ts
+++ api-gateway/src/department/department.controller.ts
@@ -1,7 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { DepartmentService } from './department.service';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/users/enums/user-role.enum';
+import { CreateDepartmentDto } from './dtos/create-department.dto';
+import { UpdateDepartmentDto } from './dtos/update-department.dto';
 
-@Controller('department')
+@Roles(UserRole.ADMIN)
+@Controller('api/v1/department')
 export class DepartmentController {
   constructor(private readonly departmentService: DepartmentService) {}
+
+  private logger = new Logger(DepartmentController.name);
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createDepartment(@Body() createDepartmentDto: CreateDepartmentDto) {
+    this.logger.log(`Department: ${JSON.stringify(createDepartmentDto)}`);
+    this.departmentService.createDepartment(createDepartmentDto);
+  }
+
+  @Get()
+  async getDepartments(@Query('id') id: string) {
+    return await this.departmentService.getDepartments(id);
+  }
+
+  @Put('/:id')
+  @UsePipes(ValidationPipe)
+  async updateDepartment(
+    @Param('id') id: string,
+    @Body() updateDepartmentDto: UpdateDepartmentDto,
+  ) {
+    this.logger.log(
+      `Department: ${id}, Body: ${JSON.stringify(updateDepartmentDto)}`,
+    );
+    await this.departmentService.updateDepartment(id, updateDepartmentDto);
+  }
+
+  @Delete('/:id')
+  async deleteDepartment(@Param('id') id: string) {
+    this.logger.log(`Delete department: ${id}`);
+    await this.departmentService.deleteDepartment(id);
+  }
 }
--- api-gateway/src/department/department.service.ts
+++ api-gateway/src/department/department.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CreateDepartmentDto } from './dtos/create-department.dto';
+import { UpdateDepartmentDto } from './dtos/update-department.dto';
 
 @Injectable()
-export class DepartmentService {}
+export class DepartmentService {
+  createDepartment(createDepartmentDto: CreateDepartmentDto) {}
+  async getDepartments(id: string) {}
+  async updateDepartment(
+    id: string,
+    updateDepartmentDto: UpdateDepartmentDto,
+  ) {}
+  async deleteDepartment(id: string) {}
+}
