03f4462d26c06425d5a0025a7346f2761e8f0a81
Felipe Lima <lipezera1836@gmail.com>
2024-12-05 09:19:11 -0300

    feat(api-gateway): implemented basic members service methods

--- api-gateway/src/members/members.module.ts
+++ api-gateway/src/members/members.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
+import { ProxyrmqModule } from 'src/proxyrmq/proxyrmq.module';
 
 @Module({
+  imports: [ProxyrmqModule],
   controllers: [MembersController],
   providers: [MembersService],
 })
--- api-gateway/src/members/members.service.ts
+++ api-gateway/src/members/members.service.ts
@@ -1,11 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { UpdateMemberDto } from './dtos/update-member.dto';
 import { CreateMemberDto } from './dtos/create-member.dto';
+import { ClientProxyCadAssemblies } from 'src/proxyrmq/client-proxy';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class MembersService {
-  async createMember(createMemberDto: CreateMemberDto) {}
-  async getMembers(id: string) {}
-  async updateMember(id: string, updateMemberDto: UpdateMemberDto) {}
-  async deleteMember(id: string) {}
+  constructor(
+    private readonly clientProxyCadAssemblies: ClientProxyCadAssemblies,
+  ) {}
+
+  private clientMembers =
+    this.clientProxyCadAssemblies.getClientMembersInstance();
+
+  private logger = new Logger(MembersService.name);
+
+  async createMember(createMemberDto: CreateMemberDto) {
+    try {
+      this.clientMembers.emit('create-member', createMemberDto);
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async getMembers(id: string) {
+    const member = await lastValueFrom(
+      this.clientMembers.send('get-members', id ? id : ''),
+    );
+
+    if (!member) {
+      throw new NotFoundException('Member not found');
+    }
+    try {
+      return member;
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async updateMember(id: string, updateMemberDto: UpdateMemberDto) {
+    const member = await lastValueFrom(
+      this.clientMembers.send('get-members', id ? id : ''),
+    );
+
+    if (!member) {
+      throw new NotFoundException('Member not found');
+    }
+
+    try {
+      this.clientMembers.emit('update-member', { id, updateMemberDto });
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async deleteMember(id: string) {
+    const member = await lastValueFrom(
+      this.clientMembers.send('get-members', id ? id : ''),
+    );
+
+    if (!member) {
+      throw new NotFoundException('Member not found');
+    }
+
+    try {
+      this.clientMembers.emit('delete-member', id);
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
 }
