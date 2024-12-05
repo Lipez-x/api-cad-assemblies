9bd16b1e22ceb5f950eaeb8b77f0ba58d8f7c75c
Felipe Lima <lipezera1836@gmail.com>
2024-12-05 09:26:21 -0300

    feat(members): create microservice members

--- micro-members/src/app.module.ts
+++ micro-members/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MembersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MembersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- micro-members/src/main.ts
+++ micro-members/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+
+const RMQ_USER = process.env.RMQ_USER;
+const RMQ_PASSWORD = process.env.RMQ_PASSWORD;
+const RMQ_ADDRESS = process.env.RMQ_ADDRESS;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${RMQ_USER}:${RMQ_PASSWORD}@${RMQ_ADDRESS}/cadassemblies`],
+      noAck: false,
+      queue: 'members',
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();
