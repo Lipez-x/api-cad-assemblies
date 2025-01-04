6933783e5a9c4598c7f08c9dafa5224b1561fd45
Felipe Lima <lipezera1836@gmail.com>
2025-01-04 00:14:57 -0300

    docs: department dtos

--- api-gateway/src/department/dtos/create-department.dto.ts
+++ api-gateway/src/department/dtos/create-department.dto.ts
@@ -1,8 +1,16 @@
 import { IsString } from 'class-validator';
 
 export class CreateDepartmentDto {
+  /**
+   * Nome do departamento
+   * @example 'Juventude'
+   */
   @IsString()
   name: string;
+  /**
+   * Líder do departamento
+   * @example 'Dc. Jorge'
+   */
   @IsString()
   leader: string;
 }
--- api-gateway/src/department/dtos/update-department.dto.ts
+++ api-gateway/src/department/dtos/update-department.dto.ts
@@ -1,10 +1,18 @@
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateDepartmentDto {
+  /**
+   * Nome do departamento
+   * @example 'Juventude'
+   */
   @IsString()
   @IsOptional()
-  name: string;
+  name?: string;
+  /**
+   * Líder do departamento
+   * @example 'Dc. Jorge'
+   */
   @IsString()
   @IsOptional()
-  leader: string;
+  leader?: string;
 }
